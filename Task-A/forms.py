f5e59d49b2e0ee21f6446c22960db94ab5a299c0
xplchanx <s3561165@student.rmit.edu.au>
2020-06-06 17:15:29 +1000

    login and logout

--- Task-A/forms.py
+++ Task-A/forms.py
@@ -3,7 +3,8 @@ Contains all the forms required to obtain data from user.
 """
 
 from flask_wtf import FlaskForm
-from wtforms import SelectField, SubmitField
+from wtforms import SelectField, SubmitField, StringField, PasswordField
+from wtforms.validators import DataRequired
 
 class RepairsForm(FlaskForm):
     engineerName = SelectField(
@@ -12,3 +13,8 @@ class RepairsForm(FlaskForm):
         
     )
     submit = SubmitField("Submit")
+
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Login")
