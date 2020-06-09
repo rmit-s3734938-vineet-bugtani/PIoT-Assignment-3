2f9af95f7749e19f5d7c5e7f5f382b7057c85800
ryanc-scalegrowth <37936511+rmit-s3740446-Ryan-Cassidy@users.noreply.github.com>
2020-06-09 15:12:45 +1000

    started implementing socketio

--- Task-A/flask_main.py
+++ Task-A/flask_main.py
@@ -1,9 +1,13 @@
 from flask import Flask, request, jsonify, render_template
+import gevent
+from gevent import monkey
+monkey.patch_all(subprocess=True)
 from flask_api import *
 from flask_admin import expose, AdminIndexView, Admin, BaseView
 from flask_admin.contrib.sqla import ModelView
 from app import site
 from database_utils import DatabaseUtils
+from socketioServer import sios
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
@@ -19,6 +23,8 @@ db.init_app(app)
 
 app.register_blueprint(api)
 app.register_blueprint(site)
+
+sios.init_app(app)
 
 class LoginView(BaseView):
     @expose('/')
@@ -37,4 +43,5 @@ admin.add_view(LoginView(name='Logout', endpoint='/logout'))
 if __name__ == "__main__":
     # with DatabaseUtils() as db:
     #     db.createTables()
-    app.run(host = "0.0.0.0", debug=True)
+    #app.run(host = "0.0.0.0", debug=True)
+    sios.run(app, host = "192.168.1.225", debug=True)
---
+++ Task-A/socketioServer.py
@@ -0,0 +1,21 @@
+from flask_socketio import SocketIO, send, emit
+from flask import request
+import time
+import requests, json
+
+sios = SocketIO()
+
+# Reset agent event
+@sios.on('reset')
+def handle_reset():
+    return
+
+# Get mac address list event
+@sios.on('maclist')
+def get_maclist():
+    return
+
+# Get engineer profile event
+@sios.on('profile')
+def get_profile(message);
+    return
