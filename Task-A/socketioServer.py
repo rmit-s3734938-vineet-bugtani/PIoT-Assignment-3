04cd30e450071f7de7fe7ce39e8dd33d21d4ca42
ryanc-scalegrowth <37936511+rmit-s3740446-Ryan-Cassidy@users.noreply.github.com>
2020-06-13 13:07:21 +1000

    engineer authorization event

--- Task-A/socketioServer.py
+++ Task-A/socketioServer.py
@@ -15,11 +15,29 @@ def handle_reset():
 def get_maclist():
     response = requests.get(request.host_url + "/deviceAddresses")
     jlist = json.loads(response.text)
-    maclist = []
+    mac_list = []
     for item in jlist:
         if item['DeviceAddress'] != 'None':
-            maclist.append(item['DeviceAddress'])
-    return maclist
+            mac_list.append(item['DeviceAddress'])
+    return mac_list
+
+# Check engineer authorization event
+@sios.on('authorize')
+def authorize_engineer(message):
+    # Get message variables
+    carID = message[0]
+    deviceaddress = message[1]
+    # Identify engineer based on mac address
+    response = requests.get(request.host_url + "/engineer/mac/" + deviceaddress)
+    engineer = json.loads(response.text)
+    # Check if engineer is registered to work on carID
+    response = requests.get(request.host_url + "/repairsByUsername/" + engineer[0]["UserName"])
+    repairs = json.loads(response.text)
+    # Return engineer name, true or false
+    for item in repairs:
+        if carID == item['CarID']:
+            return True, engineer[0]["UserName"]
+    return False, engineer[0]["UserName"]
 
 # Get engineer profile event
 @sios.on('profile')
