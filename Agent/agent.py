33eda12e6f260e35127ae8a5c29bb4fd9a37e16e
ryanc-scalegrowth <37936511+rmit-s3740446-Ryan-Cassidy@users.noreply.github.com>
2020-06-13 14:26:26 +1000

    bluetooth_auth function

--- Agent/agent.py
+++ Agent/agent.py
@@ -47,25 +47,17 @@ class agentClient:
         self.sioc.connect(self.ip)
             
     def displayMenu(self):
+        self.clear()
         print("Engineer Authnetication App")
         print("1. Engineer Bluetooth Scan")
         print("2. QR Code Profile Scan")
         print("3. Exit")
         option = input("Select an option: ")
 
+        # Get maclist for bluetooth authorization
         if option =='1':
-            # Get authorized mac addresses
-            mac_list = self.sioc.call('maclist')
-            device_address = None
-            nearby_devices = bluetooth.discover_devices()
-            for mac_address in nearby_devices:
-                if mac_address in mac_list:
-                    device_address = mac_address
-                    print("Phone Found")
-            if device_address is not None:
-                print("Mac address ==", device_address)
-            else:
-                print("Phone not found")
+            self.sioc.emit('maclist', callback= self.bluetooth_auth)
+        # Get engineer profile through QR code
         elif option == '2':
             # Detect image
                 imageName = input('Please enter image name: ')
@@ -80,11 +72,41 @@ class agentClient:
                     time.sleep(1)
                     self.displayMenu()
                     # self.sioc.emit('reset', callback = self.displayMenu)
+        # Close program
         elif option == '3':
             self.sioc.disconnect()
             sys.exit()
         else:
             sys.exit("Incorrect Input")
+    
+    def bluetooth_auth(self, mac_list):
+        # Check surrounding devices against authorized mac addresses
+        device_address = None
+        nearby_devices = bluetooth.discover_devices()
+        for mac_address in nearby_devices:
+            if mac_address in mac_list:
+                device_address = mac_address
+                print("Device Found")
+        if device_address is not None:
+            # Verify engineer authorization for car via device address
+            print("Checking engineer authorization")
+            auth, name = self.sioc.call('authorize', data=[self.carID, device_address])
+            if auth == False:
+                print("Engineer " + name + "is not authorized to work on this car")
+                print("Returning to menu")
+                time.sleep(3)
+                self.sioc.emit('reset', callback = self.displayMenu)
+            else:
+                print("Hello engineer " + name)
+                print("Device address ==", device_address)
+                print("You are authorized to perform maintenance on this car")
+                print("Car unlocked, press enter when finished with maintenance")
+                input()
+                self.sioc.emit('reset', callback = self.displayMenu)
+        else:
+            print("Authorized device not found")
+            self.sioc.emit('reset', callback = self.displayMenu)
+
 
 if __name__ == "__main__":
     agent = agentClient()
