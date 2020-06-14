cd182af047228850e25b4b7cc951f75fb81c6a36
ryanc-scalegrowth <37936511+rmit-s3740446-Ryan-Cassidy@users.noreply.github.com>
2020-06-14 17:51:07 +1000

    agent unit tests

---
+++ Agent/unittests.py
@@ -0,0 +1,86 @@
+import unittest
+import socketio
+import sys, json
+import cv2
+from pyzbar.pyzbar import decode
+
+class Agent_Tests(unittest.TestCase):
+    client = socketio.Client() #Test socket client
+    ip = None
+    carID = None
+
+    # Config and testing methods
+    def load_config(self) :
+        """ Loads json config from config.json
+        """
+        try:
+            with open("config.json") as f:
+                data = json.load(f)
+                self.ip = "http://" + data["host"] + ":" + data["port"]
+                self.carID = data["carID"]
+        except Exception as e:
+            print(str(e))
+            sys.exit("Error when reading from Json.")
+    
+    # Connect to testing namespace
+    def connect(self) : 
+        self.client.connect(self.ip, namespaces=['/test'])
+    
+    # QR method
+    def getUsernameFromQR(self, imageName):
+        # This will change - not sure whether mac needs full path or some other issues.
+        image = cv2.imread('./QRCodes/' + imageName)
+        for barcode in decode(image):
+            username = barcode.data.decode('utf-8')
+            return username
+
+    # Before All
+    @classmethod
+    def setUpClass(self):
+        self.load_config(self)
+        self.connect(self)
+    
+    # After All
+    @classmethod
+    def tearDownClass(self):
+        self.client.disconnect()
+        exit
+
+    # Tests require master server to be running
+    
+    # Test connection to Master server by retrieving SID
+    # SID is the ID assigned to a socket connection by Master
+    def test_Socket_Connection(self):
+        sid = self.client.sid
+        self.assertTrue(sid)
+    
+    # Test that authorized device address list returns
+    def test_Maclist_Return(self):
+        mac_list = self.client.call('maclist')
+        self.assertTrue(mac_list)
+    
+    # Test unauthorized mac address
+    def test_Unauth_Address(self):
+        mac_list = self.client.call('maclist')
+        mac = "TH:IS:IS:FA:KE"
+        auth = False
+        if mac in mac_list:
+            auth = True
+        self.assertFalse(auth)
+
+    # Test unknown QR code
+    def test_QR_Profile_Incorrect(self):
+        imageName = "fakename.png"
+        username = self.getUsernameFromQR(imageName)
+        found, profile = self.client.call('qr_profile', data=username)
+        self.assertFalse(found)
+    
+    # Test known QR code
+    def test_QR_Profile_Correct(self):
+        imageName = "mWoods.png"
+        username = self.getUsernameFromQR(imageName)
+        found, profile = self.client.call('qr_profile', data=username)
+        self.assertTrue(found)
+
+if __name__ == "__main__":
+    unittest.main()
